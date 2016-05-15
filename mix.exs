e23f02dba7e51b3cdc4ed61dee409c8d369115c4
Frank Hunleth <fhunleth@troodon-software.com>
2016-05-15 10:05:50 -0400

    Use elixir_make

--- mix.exs
+++ mix.exs
@@ -1,89 +1,3 @@
-defmodule Mix.Tasks.Compile.NervesUart do
-  @shortdoc "Compiles Nerves.UART C Code"
-
-  def run(_) do
-    File.mkdir("priv")
-    {exec, args} = case :os.type do
-      {:unix, os} when os in [:freebsd, :openbsd] ->
-        {"gmake", ["all"]}
-      {:win32, _} ->
-        {"mingw32-make", ["all"]}
-      _ ->
-        {"make", ["all"]}
-    end
-
-    if System.find_executable(exec) do
-      build(exec, args)
-      Mix.Project.build_structure
-      :ok
-    else
-      nocompiler_error(exec)
-    end
-  end
-
-  def build(exec, args) do
-    {result, error_code} = System.cmd(exec, args, stderr_to_stdout: true)
-    IO.binwrite result
-    if error_code != 0, do: build_error
-  end
-
-  defp nocompiler_error(exec) do
-    raise Mix.Error, message: nocompiler_message(exec) <> help_message(:os.type)
-  end
-  defp build_error() do
-    raise Mix.Error, message: build_message <> help_message(:os.type)
-  end
-
-  defp nocompiler_message(exec) do
-    """
-    Could not find the program `#{exec}`.
-
-    You will need to install the C compiler `#{exec}` to be able to build
-    Nerves.UART.
-
-    """
-  end
-
-  defp build_message do
-    """
-    Could not compile Nerves.UART.
-
-    Please make sure that you are using Erlang / OTP version 18.0 or later
-    and that you have a C compiler installed.
-
-    """
-  end
-
-  defp help_message({:win32, _}) do
-    """
-    You need to install MinGW and make sure that it is in your PATH. Test this by
-    running `gcc --version` on the command line.
-
-    If you do not have MinGW, one method to get it is to install it through
-    Chocolatey. See http://chocolatey.org to install Chocolatey and run the
-    following from and command prompt with administrative privileges:
-
-    `choco install mingw`
-    """
-  end
-  defp help_message(_) do
-    """
-    Please follow the directions below for the operating system you are
-    using:
-
-    Mac OS X: You need to have gcc and make installed. Try running the
-    commands `gcc --version` and / or `make --version`. If these programs
-    are not installed, you will be prompted to install them.
-
-    Linux: You need to have gcc and make installed. If you are using
-    Ubuntu or any other Debian-based system, install the packages
-    `build-essential`. Also install `erlang-dev` package if not
-    included in your Erlang/OTP version.
-
-    """
-  end
-end
-
 defmodule NervesUart.Mixfile do
   use Mix.Project
 
@@ -101,31 +15,23 @@ defmodule NervesUart.Mixfile do
      description: @description,
      package: package,
      source_url: "https://github.com/nerves-project/nerves_uart",
-     compilers: [:nerves_uart] ++ Mix.compilers,
+     compilers: [:elixir_make] ++ Mix.compilers,
+     make_executable: make_executable,
+     make_makefile: make_makefile,
+     make_error_message: make_error_message,
      docs: [extras: ["README.md"]],
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
+      {:elixir_make, "~> 0.1.0"},
       {:earmark, "~> 0.2", only: :dev},
       {:ex_doc,  "~> 0.11", only: :dev}
     ]
@@ -139,4 +45,36 @@ defmodule NervesUart.Mixfile do
       links: %{"GitHub" => "https://github.com/nerves-project/nerves_uart"}
     ]
   end
+
+  defp make_executable do
+    case :os.type() do
+      {:win32, _} ->
+        "mingw32-make"
+      _ ->
+        :default
+    end
+  end
+
+  defp make_makefile do
+    "Makefile"
+  end
+
+  @windows_mingw_error_msg """
+  You may need to install mingw-w64 and make sure that it is in your PATH. Test this by
+  running `gcc --version` on the command line.
+
+  If you do not have mingw-w64, one method to install it is by using
+  Chocolatey. See http://chocolatey.org to install Chocolatey and run the
+  following from and command prompt with administrative privileges:
+
+  `choco install mingw`
+  """
+
+  defp make_error_message do
+    case :os.type() do
+      {:win32, _} -> @windows_mingw_error_msg
+      _ -> :default
+    end
+  end
+
 end
