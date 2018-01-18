1a3271f9525e53e8d8fd1a4d0ef45775dc308ff7
Frank Hunleth <fhunleth@troodon-software.com>
2018-01-17 21:30:00 -0500

    Text formating/wrapping

--- lib/uart/framing.ex
+++ lib/uart/framing.ex
@@ -13,7 +13,7 @@ defmodule Nerves.UART.Framing do
   @callback init(args :: term) :: {:ok, state} | {:error, reason} when state: term, reason: term
 
   @doc """
-  Add framing to the passed in data.
+  Add framing to the passed in data.
 
   The returned `frame_data` will be sent out the UART.
   """
@@ -24,7 +24,7 @@ defmodule Nerves.UART.Framing do
                  reason: term
 
   @doc """
-  Remove the framing off received data. If a partial frame is left over at the
+  Remove the framing off received data. If a partial frame is left over at the
   end, then `:in_frame` should be returned. All of the frames received should
   be returned in the second tuple.
 
@@ -40,7 +40,7 @@ defmodule Nerves.UART.Framing do
             when new_state: term
 
   @doc """
-  If `remove_framing/2` returned `:in_frame` and a user-specified timeout for
+  If `remove_framing/2` returned `:in_frame` and a user-specified timeout for
   reassembling frames has elapsed, then this function is called. Depending on
   the semantics of the framing, a partial frame may be returned or the
   incomplete frame may be dropped.
@@ -48,7 +48,7 @@ defmodule Nerves.UART.Framing do
   @callback frame_timeout(state :: term) :: {:ok, [term], new_state} when new_state: term
 
   @doc """
-  This is called when the user invokes `Nerves.UART.flush/2`. Any partially
+  This is called when the user invokes `Nerves.UART.flush/2`. Any partially
   received frames should be dropped.
   """
   @callback flush(direction :: :receive | :transmit | :both, state :: term) :: new_state
