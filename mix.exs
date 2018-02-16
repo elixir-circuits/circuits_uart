8727885863f5552c52358d92b43f66c64d709298
Frank Hunleth <fhunleth@troodon-software.com>
2018-02-16 10:57:02 -0500

    Elixir -> 1.5, Erlang -> 20.1
    Bump Elixir requirement and the Erlang version used on Travis. Aside from
    the monotonic time parameter change, there's probably nothing preventing
    use on older versions, but those aren't receiving much or any testing
    anymore.

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule Nerves.UART.Mixfile do
     [
       app: :nerves_uart,
       version: @version,
-      elixir: "~> 1.3",
+      elixir: "~> 1.5",
       name: "Nerves.UART",
       description: @description,
       package: package(),
