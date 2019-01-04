095c3479396a0db8e1e39e3033aab01e0c684a60
Frank Hunleth <fhunleth@troodon-software.com>
2019-01-04 17:33:41 -0500

    Make running Dialyzer more convenient

--- mix.exs
+++ mix.exs
@@ -47,7 +47,8 @@ defmodule Circuits.UART.MixProject do
   defp deps() do
     [
       {:elixir_make, "~> 0.4", runtime: false},
-      {:ex_doc, "~> 0.19", only: [:dev, :test], runtime: false}
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false},
+      {:dialyxir, "1.0.0-rc.4", only: :dev, runtime: false}
     ]
   end
 
