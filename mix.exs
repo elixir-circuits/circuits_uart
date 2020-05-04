dc0e4221f1a9d3a3cd2c04789d0c5b0364cbcf9b
Frank Hunleth <fhunleth@troodon-software.com>
2020-05-04 13:13:36 -0400

    Bump Dialyxir to 1.0.0

--- mix.exs
+++ mix.exs
@@ -53,7 +53,7 @@ defmodule Circuits.UART.MixProject do
     [
       {:elixir_make, "~> 0.6", runtime: false},
       {:ex_doc, "~> 0.19", only: :dev, runtime: false},
-      {:dialyxir, "~> 1.0.0-rc.4", only: :dev, runtime: false}
+      {:dialyxir, "~> 1.0.0", only: :dev, runtime: false}
     ]
   end
 
