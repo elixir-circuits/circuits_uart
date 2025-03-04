503674e3a05a9212359860b6ff2cde9f5023ef74
Frank Hunleth <fhunleth@troodon-software.com>
2025-03-04 09:18:35 -0500

    Make Ericsson copyright/license REUSE compliant

--- src/ei_copy/decode_intlist.c
+++ src/ei_copy/decode_intlist.c
@@ -1,22 +1,7 @@
-/*
- * %CopyrightBegin%
- * 
- * Copyright Ericsson AB 1998-2009. All Rights Reserved.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * 
- * %CopyrightEnd%
- */
+// SPDX-FileCopyrightText: Ericsson AB 1998-2013. All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+
 /* since Erlang sends int-lists as either lists or strings, this
  * function can be used when the caller needs an array but doesn't
  * know which type to decode
